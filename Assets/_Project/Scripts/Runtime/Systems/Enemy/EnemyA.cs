c202e5683e37aac67e656eed2d950a4a829148b6
Erick Araujo <erickequipe7@gmail.com>
2023-09-20 10:04:40 -0300

    Added states

--- Assets/_Project/Scripts/Runtime/Systems/Enemy/EnemyA.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Enemy/EnemyA.cs
@@ -10,7 +10,6 @@ public enum EnemyState
     Escape
 }
 
-
 public class EnemyA : MonoBehaviour
 {
     public EnemyState currentState;
@@ -24,16 +23,31 @@ public class EnemyA : MonoBehaviour
 
     private Vector3 dirPlayer;
 
-    public bool isLookLeft;
-    public bool canHit;
-    public bool canAttack;
-
+    #region Base
+    [Header("base")]
     [SerializeField] private float speed;
+    [SerializeField] private float maxDistancePlayer;
+    [SerializeField] private float percStopAttack;
+    [SerializeField] private bool isLookLeft;
+    [SerializeField] private bool canHit;
+    #endregion
+
+    #region Chase
+    [Header("Chase")]
     [SerializeField] private float delayTime;
     [SerializeField] private float delayAttack;
     [SerializeField] private float delayNewAttack;
-    [SerializeField] private float maxDistancePlayer;
-    [SerializeField] private float percStopAttack;
+    [SerializeField] private float chaseTime;
+    [SerializeField] private bool canAttack;
+    [SerializeField] private bool isChase;
+    #endregion
+
+    [Header("Escape")]
+    [SerializeField] private float safeDistance;
+    [SerializeField] private float minTime,maxTime;
+    private float timeTemp;
+    private bool isWalk;
+    private float walkTime;
 
     private void Start()
     {
@@ -44,6 +58,19 @@ public class EnemyA : MonoBehaviour
 
     private void Update()
     {
+        switch (currentState)
+        {
+            case EnemyState.Patrol:
+                Patrol();
+                break;
+            case EnemyState.Chase:
+                Chase();
+                break;
+            case EnemyState.Escape:
+                Escape();
+                break;
+        }
+
         Movement();
     }
 
@@ -51,18 +78,66 @@ public class EnemyA : MonoBehaviour
     {
         posXplayer = playerController.transform.position.x;
         posZplayer = playerController.transform.position.z;
-
         dirPlayer = playerController.transform.position - transform.position;
+
+        if (float.IsNaN(movHorizontal))
+            movHorizontal = 0;
+        if (float.IsNaN(movVertical))
+            movVertical = 0;
+
+        enemyRb.velocity = new Vector3(movHorizontal * speed, enemyRb.velocity.y, movVertical * speed);
+    }
+
+
+    #region States
+    private void Patrol()
+    {
+        float perc = safeDistance * 0.5f;
+
+        if (MathF.Abs(dirPlayer.x) <= perc)
+        {
+            currentState = EnemyState.Chase;
+            if (!isChase)
+            {
+                isChase = true;
+                StartCoroutine(EndChase());
+            }
+        }
+
+        timeTemp += Time.deltaTime;
+
+        if (!isWalk)
+        {
+            movHorizontal = Random.Range(-1, 2);
+            movVertical = Random.Range(-1, 2);
+            walkTime = Random.Range(minTime, maxTime);
+            isWalk = true;
+
+            if (movHorizontal < 0 && !isLookLeft)
+            {
+                Flip();
+            }
+            else if (movHorizontal > 0 && isLookLeft)
+            {
+                Flip();
+            }
+        }
+
+        if (timeTemp >= walkTime)
+        {
+            timeTemp = 0;
+            isWalk = false;
+        } 
+    }
+
+    private void Chase()
+    {
+        FlipController();
 
         if (!canHit && !canAttack)
         {
             movHorizontal = dirPlayer.x / Mathf.Abs(dirPlayer.x);
             movVertical = dirPlayer.z / Mathf.Abs(dirPlayer.z);
-
-            if (float.IsNaN(movHorizontal))
-                movHorizontal = 0;
-            if (float.IsNaN(movVertical))
-                movVertical = 0;
         }
         else
         {
@@ -86,9 +161,36 @@ public class EnemyA : MonoBehaviour
         {
             StopAttack();
         }
+    }
 
-        enemyRb.velocity = new Vector3(movHorizontal * speed, enemyRb.velocity.y, movVertical * speed);
+    private void Escape()
+    {
+        FlipController();
 
+        if (MathF.Abs(dirPlayer.x) <= safeDistance)
+        {
+            movHorizontal = dirPlayer.x / Mathf.Abs(dirPlayer.x) * -1;
+            movVertical = 0;
+        }
+        else
+        {
+            currentState = EnemyState.Patrol;
+        }
+    }
+
+    #endregion
+
+    private IEnumerator EndChase()
+    {
+        print("chamei aqui");
+        yield return new WaitForSeconds(chaseTime);
+        print("esperei o tempo");
+        isChase = false;
+        currentState = EnemyState.Escape;
+    }
+
+    private void FlipController()
+    {
         if (transform.position.x < posXplayer && isLookLeft)
         {
             Flip();
@@ -97,39 +199,7 @@ public class EnemyA : MonoBehaviour
         {
             Flip();
         }
-
-        switch (currentState)
-        {
-            case EnemyState.Patrol:
-                Patrol();
-                break;
-            case EnemyState.Chase:
-                Chase();
-                break;
-            case EnemyState.Escape:
-                Escape();
-                break;
-        }
     }
-
-
-    #region States
-    private void Patrol()
-    {
-
-    }
-
-    private void Chase()
-    {
-
-    }
-
-    private void Escape()
-    {
-
-    }
-
-    #endregion
 
     private void Flip()
     {
